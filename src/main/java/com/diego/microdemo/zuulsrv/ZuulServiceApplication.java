c4758a97acbc070154d37ed7573ec387830c0cc5
diegochavezcarro <diegochavezcarro@gmail.com>
2019-09-23 18:26:19 -0300

    ahora el zuul no deja pasar si no se esta autenticado

--- src/main/java/com/diego/microdemo/zuulsrv/ZuulServiceApplication.java
+++ src/main/java/com/diego/microdemo/zuulsrv/ZuulServiceApplication.java
@@ -3,7 +3,9 @@ package com.diego.microdemo.zuulsrv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulServiceApplication {
